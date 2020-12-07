14f2b91827d86d83dbd2d8ea4dfd7d7ac90b19b2
VictorCardial <victor_cardial@hotmail.com>
2020-12-07 05:55:55 +0000

    aula5

---
+++ src/components/Output.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>{{ numero1 * 1 + numero2 * 1 }}</p>
+    <p>{{ numero1 - numero2 }}</p>
+    <p>{{ numero1 * numero2 }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "Output",
+  props: {
+    numero1: Number,
+    numero2: Number,
+  },
+};
+</script>
